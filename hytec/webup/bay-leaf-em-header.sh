7bf35ff1a4e016e7a0d4f64899292a7e900ab0ac
Bret Njagi <49677496+m0th3rch1p@users.noreply.github.com>
2021-12-28 18:37:25 +0300

    Delete bay-leaf-em-header.sh

--- hytec/webup/bay-leaf-em-header.sh
+++
@@ -1,8 +0,0 @@
-if [ $DayGreet -lt 12 ]; then                                                                 # Time sensitve greetings
-  echo "Good morning:" > $SiteReport1                                                         # Also if needed - they are often not, but hay ho
-elif [ $DayGreet -lt 18 ]; then
-  echo "Good afternoon:" > $SiteReport1
-else
-  echo "Good evening:" > $SiteReport1
-fi
-  echo " " > $SiteReport1
